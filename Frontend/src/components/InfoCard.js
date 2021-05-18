a53d8024edc2d8911684381e2563577fe636c720
Calvin Dong <calvin.dong156@gmail.com>
2021-05-18 16:20:38 +1000

    - backend setup

---
+++ Frontend/src/components/InfoCard.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+export default function InfoCard(props){
+  const placeholder = 0;
+
+  return(
+    <header>This is just here for now</header>
+  )
+}
